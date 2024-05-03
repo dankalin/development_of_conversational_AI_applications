35a26b1765e7cf956af860a0e7f291c5ff61fed3
dbashkirov <bashkirovdp@gmail.com>
2024-05-04 00:36:19 +0300

    added buttons

--- bot.py
+++ bot.py
@@ -1,18 +1,28 @@
 import asyncio
 from telebot.async_telebot import AsyncTeleBot
+from telebot import types
 from dotenv import load_dotenv
 import os
 import requests
 
 load_dotenv()
 
-URL = "http://0.0.0.0"
-PORT = "8080"
+URL = "https://mandate-ashley-municipal-problems.trycloudflare.com"
 
 
 token = os.getenv("API_TOKEN")
 
 bot = AsyncTeleBot(token)
+
+kb = types.InlineKeyboardMarkup([
+    [
+        types.InlineKeyboardButton(text='1', callback_data='btn_types'),
+        types.InlineKeyboardButton(text='2', callback_data='btn_types'),
+        types.InlineKeyboardButton(text='3', callback_data='btn_types'),
+        types.InlineKeyboardButton(text='4', callback_data='btn_types'),
+        types.InlineKeyboardButton(text='5', callback_data='btn_types'),
+    ]
+])
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['start'])
@@ -26,8 +36,9 @@ async def send_welcome(message):
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 async def message(message):
-    res = requests.post(f"{URL}:{PORT}/new_find_similar_saiga")
-    await bot.reply_to(message, res.text)
+    print(message.text)
+    res = requests.post(f"{URL}/new_find_similar_saiga", json={"text": message.text})
+    await bot.reply_to(message, res.json()["prediction"], reply_markup=kb)
 
 
-asyncio.run(bot.pooling())
+asyncio.run(bot.polling())
