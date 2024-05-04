5d96f6127c86bac28f3e5922e564d16fa83a821c
dbashkirov <bashkirovdp@gmail.com>
2024-05-04 16:54:23 +0300

    blacked

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
-from constants import label2text
+from src.constants import label2text
 import psycopg2
 import os
 
@@ -6,6 +6,7 @@ db_host = os.getenv("DB_HOST")
 db_name = os.getenv("DB_NAME")
 db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
+
 
 def answer_with_label(text, label):
     ans = f"""*Тема: {label2text[label]}*
@@ -15,19 +16,19 @@ def answer_with_label(text, label):
 _Пожалуйста, оцените мой ответ:_"""
     return ans
 
+
 def save_rating_to_db(user_name, rating, user_message):
     try:
         conn = psycopg2.connect(
-            host=db_host,
-            database=db_name,
-            user=db_user,
-            password=db_password
+            host=db_host, database=db_name, user=db_user, password=db_password
         )
 
         cur = conn.cursor()
 
-        cur.execute("INSERT INTO statistic (user_name, rating, message) VALUES (%s, %s, %s)",
-                    (user_name, rating, user_message))
+        cur.execute(
+            "INSERT INTO statistic (user_name, rating, message) VALUES (%s, %s, %s)",
+            (user_name, rating, user_message),
+        )
 
         conn.commit()
         cur.close()
